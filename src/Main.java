7867d939637018e0fa6f9c9e42154baf4390c31c
Valentin Kastberger <valentinkastberger98@gmail.com>
2024-05-17 16:07:44 +0200

    add java proj to git repo

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static final int TRIAL_ROUNDS = 10000000;
+    public static final int SLEEP_DURATION = 1;
+
+    public static boolean stringEquals(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                return false;
+            }
+            /*try {
+                Thread.sleep(SLEEP_DURATION);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }*/
+        }
+        return true;
+    }
+
+    public static long timedEquals(String a, String b) {
+        long startTime = System.nanoTime();
+        stringEquals(a, b);
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+    public static String bruteForce(int passwordLength, String secretString) {
+        StringBuilder guessedPassword = new StringBuilder();
+
+        for (int i = 0; i < passwordLength; i++) {
+            System.out.println("Guessed Password: " + guessedPassword);
+
+            Map<Character, Long> candidates = new HashMap<>();
+
+            for (char c = 'a'; c <= 'z'; c++) {
+                String testval = guessedPassword.toString() + c;
+                for (int j = 0; j < passwordLength - guessedPassword.length() - 1; j++) {
+                    testval += "a";
+                }
+
+                long totalTime = 0;
+                for (int x = 0; x < TRIAL_ROUNDS; x++) {
+                    long timeTaken = timedEquals(testval, secretString);
+                    totalTime += timeTaken;
+                }
+
+                long timeAvg = totalTime / TRIAL_ROUNDS;
+                candidates.put(c, timeAvg);
+            }
+
+            // Find the key with the highest value
+            Character maxKey = null;
+            long maxValue = Long.MIN_VALUE;
+
+            for (Map.Entry<Character, Long> entry : candidates.entrySet()) {
+                if (entry.getValue() > maxValue) {
+                    maxValue = entry.getValue();
+                    maxKey = entry.getKey();
+                }
+            }
+            System.out.println("Max Key: " + maxKey);
+            System.out.println("Candidates: " + candidates);
+            guessedPassword.append(maxKey);
+        }
+        return guessedPassword.toString();
+    }
+
+    public static void main(String[] args) {
+        String password = "abba";
+        int passwordLength = password.length();
+
+        String guessedPassword = bruteForce(passwordLength, password);
+        System.out.println("I'm guessing: " + guessedPassword);
+    }
+}
